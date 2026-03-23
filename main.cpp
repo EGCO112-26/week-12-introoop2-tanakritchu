d4cf883552764d18e7d04ebcdc37cd87bf924de8
tanakritchu <tanakrit.chu@student.mahidol.edu>
2026-03-23 09:43:04 +0000

    Week-12

--- main.cpp
+++ main.cpp
@@ -2,74 +2,49 @@
 using namespace std;
 
 #include "student.h"
-#define N 3
 int main(int argc, char* argv[]) {
-  student s1,s2;
 
-//  s1.set_name("Amy"); s1.set_age(20);
-//  s2.set_name("James");
-//  s2.set_age(17);
-//  s1.get_age() > s2.get_age() ? (s1.print_name(), cout << " is older") : (s2.print_name(), cout << " is older");
-
-
-
-  student a[N];
+  student a[(argc-1)/2];
   // Set age and set name for the first two ppl
-  int i,age;
+  int i,age,u;
   string n;
    // 1. Ask for N names and N age using cin
-    //  for(i = 0; i < N; i++){
-    //     cout << "Enter name: ";
-    //     cin >> n;
-    //     cout << "Enter age: ";
-    //     cin >> age;
-
-    //     a[i].set_name(n);
-    //     a[i].set_age(age);
-    // }
-
-  //2. Print name and age of all N ppl
-  for(i = 0; i < N; i++){
-        cout << "Name: " << a[i].get_name() << ", Age: " << a[i].get_age() << endl;
-    }
+for(i=0;i<(argc-1)/2;i++){
+  a[i].set_name(argv[i*2+1]);
+  a[i].set_age(atoi(argv[i*2+2]));
+ }
   
-  //3. Print name and age of 
+ 
+  //2. Print name and age of all N ppl
+//for(i=0;i<(argc-1)/2;i++){
+//  a[i].print_info();
+// }
+  
+//3. Print name and age of 
   //the youngest person
   //Find what the age of the youngest person
+int youngest = a[0].get_age();
+
+for(int i = 0; i < (argc-1)/2; i++) {
+    if (youngest > a[i].get_age()) {
+        youngest = a[i].get_age();
+    }
+}
+ // Print all info for the yougest person
+cout<<"Youngest student"<<endl;
+for(int i = 0; i < (argc-1)/2; i++) {
+    if (youngest == a[i].get_age()) {
+        a[i].print_info();
+    }
+}
+  
   // Print all info for the yougest person
-  int minAge = a[0].get_age();
-
-for(i = 1; i < N; i++){
-    if(a[i].get_age() < minAge){
-        minAge = a[i].get_age();
-    }
-}
-
-cout << "\nYoungest student(s):\n";
-for(i = 0; i < N; i++){
-    if(a[i].get_age() == minAge){
-        a[i].print_name();
-        cout << " " << a[i].get_age() << endl;
-    }
-}
  
   
-  
   //4. Change input from cin to argv
-  if(argc == 1 + 2*N){
-        for(i = 0; i < N; i++){
-            a[i].set_name(argv[1 + 2*i]);
-            a[i].set_age(atoi(argv[2 + 2*i]));
-        }
 
-        for(i = 0; i < N; i++){
-            a[i].print_name();
-            cout << " " << a[i].get_age() << endl;
-        }
-    }
-
-    return 0;
-}
   
+ return 0;
 
-
+    
+  }
--- student.h
+++ student.h
@@ -3,30 +3,33 @@ using namespace std;
 
 class student{
 private:
-
   string name;
   int age;
-
 public:
-  
-void set_name(string n){
-        name = n;
-    }
-
-    void set_age(int a){
-        age = a;
-    }
-
-    string get_name(){
-        return name;
-    }
-
-    int get_age(){
-        return age;
-    }
-
-    // print name
-    void print_name(){
-        cout << name;
-    }
+  void set_name(string="TAWAN");
+  void print_name();
+  void set_age(int );
+  int get_age();
+  void print_info();
 };
+
+void student::set_age(int a){
+  age = a;
+}
+
+void student::set_name(string x){
+  name = x;
+}
+
+void student::print_name(){
+  cout<<"Name: "<<name<<endl;
+}
+
+int student::get_age(){
+  return age;
+}
+
+void student::print_info(){
+  print_name();
+  cout<<"Age: "<<age<<endl;
+}
